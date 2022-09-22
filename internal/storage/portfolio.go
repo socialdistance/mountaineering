85dace94ca8d83dbb2267756be65c8daabeb4da3
socialdistance <pleaseuseurbrain@protonmail.com>
2022-09-22 17:30:11 +0300

    fix struct

--- internal/storage/portfolio.go
+++ internal/storage/portfolio.go
@@ -1,1 +1,12 @@
 package storage
+
+import "github.com/gofrs/uuid"
+
+type Portfolio struct {
+	ID           uuid.UUID `db:"id" json:"id"`
+	Name         string    `db:"name" json:"name"`                           // имя мастера
+	Photo        string    `db:"photo" json:"photo"`                         // путь до файла с фотографией
+	Video        string    `db:"video" json:"video"`                         // путь до файла с видео
+	Achievements string    `db:"achievements" json:"achievements,omitempty"` // достижения, что было выполнено
+	Description  string    `db:"description" json:"description,omitempty"`   // Описание
+}
